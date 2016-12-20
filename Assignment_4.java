c69f175f4c0ce69735b09267a77f3ce563b55e21
prapul <prapulpoonacha@gmail.com>
2016-12-20 13:02:16 +0530

    ascii to character
    program which converts ascii value to the character values

---
+++ Assignment_4.java
@@ -0,0 +1,13 @@
+package prapul;
+
+public class Assignment_4 {
+	public static void main(String[] args) {
+		int i;
+		for(i=65;i<=90;i++)
+		{
+			char a = (char)i;
+			System.out.println(i+" : "+a);
+		}
+	}
+
+}
